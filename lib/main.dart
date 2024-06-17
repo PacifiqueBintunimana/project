48a74a087aab1567e2b5a4f1cbb106a4e2b85e19
PC <pacifique.bintunimana@gmail.com>
2024-06-17 11:58:40 +0200

    finished project

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main(){
+print("hello world");
+
+runApp(
+
+    MaterialApp(
+    home: Scaffold(
+      body:Center(
+        child: Text("hello world")
+      )
+    )
+  )
+);
+
+}
+
+
